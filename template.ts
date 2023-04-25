f25c5ce67d401eb68906c9b64d8c00d40ffcb14a
onlymisaky <onlymisaky@qq.com>
2023-04-25 16:37:20 +0800

    Update template.ts

--- template.ts
+++ template.ts
@@ -1,3 +1,8 @@
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Selection
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Range
+// https://juejin.cn/post/6844904038991921166
+// https://zhuanlan.zhihu.com/p/159702888
+// https://blog.csdn.net/weixin_43809685/article/details/124858446
 // https://juejin.cn/post/6854573210638221320
 function getRangeOffsetInContainer(containerNode: Node, rangeNode: Node, offset: number) {
   let index = offset;
