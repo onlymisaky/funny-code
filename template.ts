1ce8abef61f0996604aa3edcf720ed76bcac6fc6
onlymisaky <onlymisaky@qq.com>
2023-05-05 13:59:16 +0800

    Update template.ts

--- template.ts
+++ template.ts
@@ -22,3 +22,112 @@ function getRangeOffsetInContainer(containerNode: Node, rangeNode: Node, offset:
   }
   return index;
 }
+
+
+export function closest(dom: Node, selectors: keyof HTMLElementTagNameMap): Node {
+  const nodes = document.querySelectorAll(selectors);
+
+  function find(node: Node) {
+    const parent = node.parentNode;
+    if (parent) {
+      for (const iterator of nodes) {
+        if (iterator === parent) {
+          return parent;
+        }
+      }
+      return find(parent);
+    }
+    return null;
+  }
+
+  return find(dom);
+}
+
+
+/**
+ * @param {Node} containerNode
+ * @param {Node} startNode
+ * @param {Node} endNode
+ * @param {string} color
+ */
+export function connectNode(containerNode, startNode, endNode, color) {
+  const {
+    offsetLeft: startX,
+    offsetTop: startY,
+    offsetWidth: startW,
+    offsetHeight: startH,
+  } = startNode;
+
+  const {
+    offsetLeft: endX,
+    offsetTop: endY,
+    offsetWidth: endW,
+    offsetHeight: endH,
+  } = endNode;
+
+  const lineW = 2;
+
+  let startLineX = startX + startW / 2;
+  let startLineY = 0;
+  const startLineH = 20;
+
+  let endLineX = endX + endW / 2;
+  let endLineY = 0;
+  let endLineH = 0;
+
+
+  let connectLineX = Math.min(startLineX, endLineX);
+  let connectLineY = 0;
+  let connectLineW = Math.abs(startLineX - endLineX);
+
+  if (startY === endY) {
+    startLineY = startY - startLineH;
+    endLineY = startLineY;
+    endLineH = startLineH;
+    connectLineY = startLineY;
+  }
+
+  if (startY > endY) {
+    startLineY = startY - startLineH;
+    endLineY = endY + endH;
+    endLineH = startLineY - endLineY;
+    connectLineY = startLineY;
+  }
+
+  if (startY < endY) {
+    startLineY = startY + startH;
+    endLineY = startLineY + startLineH;
+    endLineH = endY - endLineY;
+    connectLineY = endLineY;
+  }
+
+  [
+    {
+      left: `${startLineX}px`,
+      top: `${startLineY}px`,
+      width: `${lineW}px`,
+      height: `${startLineH}px`,
+    },
+    {
+      left: `${endLineX}px`,
+      top: `${endLineY}px`,
+      width: `${lineW}px`,
+      height: `${endLineH}px`,
+    },
+    {
+      left: `${connectLineX}px`,
+      top: `${connectLineY}px`,
+      width: `${connectLineW}px`,
+      height: `${lineW}px`,
+    },
+  ].forEach((styles) => {
+    const div = document.createElement('div');
+    div.className = 'connect-line'
+    div.style.position = 'absolute';
+    div.style.backgroundColor = color;
+    Object.keys(styles).forEach(
+      (key) => (div.style[key] = styles[key]),
+    );
+    containerNode.appendChild(div);
+  });
+}
